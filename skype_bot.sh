54a7f238aeef89e75c96a776a6e748573a4284c8
Steven Foong <foongws@ecsmy.com>
2020-06-09 16:16:29 +0800

    Update skype_bot.sh

--- skype_bot.sh
+++ skype_bot.sh
@@ -2,12 +2,17 @@
 
 export SKYPE_USERNAME="[skype username]"
 export SKYPE_PASSWORD="[skype password]"
+export ROCKETCHAT_URL="[rocket chat api url]"
 clear
 
 case $1 in
 
   list-recent-chat)
     ./skype_list_recent.chat.py
+    ;;
+  
+  rocketchat-bridge)
+    ./skype-rocketchat-bridge.py
     ;;
   
   *)
