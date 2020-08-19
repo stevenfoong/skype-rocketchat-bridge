c1b9873b78e9676b13cd5d0459b2027da80331d6
Steven Foong <foongws@ecsmy.com>
2020-08-19 16:10:47 +0800

    Update skype-rocketchat-bridge.py

--- skype-rocketchat-bridge.py
+++ skype-rocketchat-bridge.py
@@ -8,6 +8,7 @@ import magic
 import html
 import markdown_strings as md
 import pytz
+import re
 
 from skpy import Skype
 from skpy import SkypeEventLoop
@@ -55,12 +56,6 @@ if not rocketchat_x_user_id:
     print("missing config ROCKETCHAT_X_User_Id")
     sys.exit(1)
 
-# Remove to show debug info
-debug = "true"
-
-# print(f"username: {app_username}")
-# print(f"password: {app_password}")
-
 sk=Skype(app_username, app_password,".tokens")
 sk.chats
 
@@ -79,33 +74,19 @@ def process_msg(msg):
     msg = msg.replace("<pre raw_pre=\"{code}\" raw_post=\"{code}\">","")
     msg = msg.replace("</pre>","")
 
+    msg = re.sub('<(/)?ss( type="\w+")?>', '', msg)
+    msg = msg.replace("</ss>","")
+
     return msg
 
-def quote_msg(msg,msg_id,msg_channel):
-    #global quote_author, quote_msg_timestamp
-    #soup = BeautifulSoup(msg, 'html.parser')
-    #print (soup.find('legacyquote').next_sibling)
+def process_quote_msg(msg,msg_id,msg_channel):
+    soup = BeautifulSoup(msg, 'html.parser')
     msg_array = msg.split()
-    #print (msg_array)
-    q_msg = msg_array[3]
-    #print ("quote orig msg id:")
-    #print (quote_orig_msg_id)
+    q_msg = (soup.find('legacyquote').next_sibling)
     for msgs in sk.chats[msg_channel].getMsgs():
-        #print(msgs.id)
         if msgs.id == msg_id:
-            #print ("msg id:")
-            #print(msgs.id)
-            #print ("msg content:")
-            print(msgs.content)
-            soup = BeautifulSoup(msgs.content, 'html.parser')
-            if len(soup.find_all('quote')) > 0:
-                q_msg = soup.find('quote').next_sibling
-            else:
-                q_msg = msgs.content
-    soup_quote = BeautifulSoup(q_msg, 'html.parser')
-    print(q_msg)
-    #qmsg = send_msg + "```&#x25B6; \n &#09;" + soup_quote.get_text() + "\n &#09;" + quote_author + " , " + quote_msg_timestamp + "```"
-    # qmsg = msg + "\n &#62; quote msg \n" + quote_author + " , " + quote_msg_timestamp 
+            q_msg = process_msg(msgs.content)
+
     return q_msg
 
 
@@ -118,33 +99,52 @@ class MySkype(SkypeEventLoop):
             else:
                 logF = open("skype-bot.log", "w")
 
+            log_prefix = event.msg.id
+
+            logF.write(log_prefix + ": Start processing message." + '\n')
+            logF.write(log_prefix + ": Message ID : " + event.msg.id + '\n')
+            logF.write(log_prefix + ": Message type : " + event.msg.type + '\n')
+
+            if hasattr(sk.contacts.user(event.msg.userId),'name'):
+                sender_name = str(getattr(sk.contacts.user(event.msg.userId), 'name'))
+                logF.write(log_prefix + ": Sender Id found : " + event.msg.userId + '\n')
+                logF.write(log_prefix + ": Sender name found : " + sender_name + '\n')
+            else:
+                sender_name = event.msg.userId
+
             if hasattr(sk.contacts.user(event.msg.userId),'avatar'):
-                #print("Avatar :",sk.contacts.user(event.msg.userId).avatar)
                 sender_avatar = sk.contacts.user(event.msg.userId).avatar
+                logF.write(log_prefix + ": Sender avatar found : " + sender_avatar + '\n')
             else:
                 sender_avatar = "https://vignette.wikia.nocookie.net/logopedia/images/f/fb/Skype_Logo_2019.svg/revision/latest/scale-to-width-down/340?cb=20191207211056"
 
-            if hasattr(sk.contacts.user(event.msg.userId),'name'):
-                #print("Name :",sk.contacts.user(event.msg.userId).name)
-                sender_name = str(getattr(sk.contacts.user(event.msg.userId), 'name'))
-            else:
-                sender_name = event.msg.userId
-            
             if hasattr(sk.chats.chat(event.msg.chatId),'topic'):
                 chattopic=sk.chats.chat(event.msg.chatId).topic
-                #print("Chat Topic : ",chattopic)
+                logF.write(log_prefix + ": Chat Topic found : " + chattopic + '\n')
             else:
                 chattopic="skype-bot-private-message"
             
             message_content = event.msg.content
+            logF.write(log_prefix + ": Message Content : " + message_content + '\n')
             processed_message = process_msg(message_content)
+
+            data_set = {
+                "text": processed_message,
+                "avatar": sender_avatar,
+                "channel": chattopic,
+                "from":{
+                    "id":event.msg.userId,
+                    "name":sender_name
+                },
+                "conversation": {
+                    "id":event.msg.chatId
+                }
+            }
 
             if event.msg.type =="RichText/UriObject" or event.msg.type =="RichText/Media_GenericFile":
 
-                if debug:
-                    print("Message File:",event.msg.file)
-                    print("Message File Name:",event.msg.file.name)
-
+                logF.write(log_prefix + ": Message File Name : " + event.msg.file.name + '\n')
+                
                 file_attach = open(event.msg.file.name,"wb")
                 file_attach.write(event.msg.fileContent)
                 file_attach.close()
@@ -163,73 +163,45 @@ class MySkype(SkypeEventLoop):
                 room_id = room['_id']
 
                 api_url = 'https://chat.majasolutions.net/api/v1/rooms.upload/'+room_id
-                # print (magic.from_file(event.msg.file.name, mime=True))
                 files = { 'file': (event.msg.file.name, open(event.msg.file.name, 'rb'),magic.from_file(event.msg.file.name, mime=True)),}    
                 bot_msg = {'msg' : 'File ' + magic.from_file(event.msg.file.name) + ' sent by ' + sender_name}
                 upload_file = requests.post(api_url, data = bot_msg, headers = headers, files = files)
-                #print(upload_file.text)
+                logF.write(log_prefix + ": Upload File Status : " + str(upload_file.status_code) + '\n')
                 if os.path.exists(event.msg.file.name):
                     os.remove(event.msg.file.name)
 
-            SoupText = BeautifulSoup(message_content, 'html.parser')
-            if len(SoupText.find_all('quote')) > 0:
-                attributes_dictionary = SoupText.find('quote').attrs
-                tz = pytz.timezone('Asia/Kuala_Lumpur')
-                ts = datetime.fromtimestamp(int(attributes_dictionary['timestamp']), tz)
-                quote_orig_msg_id = attributes_dictionary['messageid']
-                quote_author = attributes_dictionary['authorname']
-                quote_msg_timestamp = ts.strftime('%Y-%m-%d %H:%M:%S %Z')
-                quote_channel = event.msg.chatId
-                print ("real quote message :",quote_msg(message_content,quote_orig_msg_id,quote_channel))
+            else:
+                SoupText = BeautifulSoup(processed_message, 'html.parser')
+                if len(SoupText.find_all('quote')) > 0:
+                    attributes_dictionary = SoupText.find('quote').attrs
+                    tz = pytz.timezone('Asia/Kuala_Lumpur')
+                    ts = datetime.fromtimestamp(int(attributes_dictionary['timestamp']), tz)
+                    quote_orig_msg_id = attributes_dictionary['messageid']
+                    quote_author = attributes_dictionary['authorname']
+                    quote_msg_timestamp = ts.strftime('%Y-%m-%d %H:%M:%S %Z')
+                    quote_channel = event.msg.chatId
 
-            data_set = {
-                #"text": html.unescape(event.msg.content),
-                #"text": html.unescape(format_msg(tag_format,soup.get_text())),
-                "text": processed_message,
-                "avatar": sender_avatar,
-                "channel": chattopic,
-                "from":{
-                    "id":event.msg.userId,
-                    "name":sender_name
-                },
-                "conversation": {
-                    "id":event.msg.chatId
-                }
-            }
+                    new_msg = SoupText.find('quote').next_sibling
+                    quote_msg = process_quote_msg(processed_message,quote_orig_msg_id,quote_channel)
 
-            json_dump = json.dumps(data_set)
-            #print(json_dump)
-            if event.msg.userId != skype_bot_id:
-                r=requests.post(rocketchat_url,data=json_dump)
-                if r.status_code != 200:
-                    logF.write(" Send Message failed. \n")
-                    # print("failed")
+                    data_set_attachment = {
+                        "text": new_msg,
+                        "attachment": {
+                            "color": '#0000DD',
+                            "title": quote_author + " @ " + quote_msg_timestamp,
+                            "text": quote_msg
+                        }
+                    }
+                    data_set.update(data_set_attachment)
 
-                    logF.write("\n")
+                json_dump = json.dumps(data_set)
+                logF.write(log_prefix + ": Message Sent : " + json_dump + '\n')
 
-            if debug:
-                print("Chat Topic :",chattopic)
-                print("Chat ID :",event.msg.chatId)
-                print("Message ID :",event.msg.id)
-                print("User ID :",event.msg.userId)
-                print("User Name :",sender_name)
-                print("User Avartar :",sender_avatar)
-                #print("Message :",event.msg.content)
-                print("Message :",message_content)
-                print("Message Type:",event.msg.type)
-                print("Message Sent:",processed_message)
-                
-
-            logF.write("Chat Topic :" + chattopic + '\n')
-            logF.write("Chat ID :" + event.msg.chatId + '\n')
-            logF.write("Message ID :" + event.msg.id + '\n')
-            logF.write("User ID :" + event.msg.userId + '\n')
-            logF.write("User Name :" + sender_name + '\n')
-            logF.write("User Avartar :" + sender_avatar + '\n')
-            logF.write("Message :" + message_content + '\n')
-            logF.write("Message Type :" + event.msg.type + '\n')
-            logF.write("Message Sent :" + processed_message + '\n')
-
+                if event.msg.userId != skype_bot_id:
+                    send_message = requests.post(rocketchat_url,data=json_dump)
+                    logF.write(log_prefix + ": Send Message Status : " + str(send_message.status_code) + '\n')
+                    
+            logF.write("\n")
             logF.close()
 
 sk = MySkype(tokenFile=".tokens", autoAck=True)
